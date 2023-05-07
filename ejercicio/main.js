66e3bba63085a185f78fcf19a73cf418a8b8e16f
Fernando <fernando.m.lamacchia@hotmail.com>
2023-05-07 20:00:47 -0300

    ejercicioJS

---
+++ ejercicio/main.js
@@ -0,0 +1,8 @@
+let nombre = prompt ("Ingrese su nombre");
+let apellido = prompt ("Ingrese su Apellido");
+let numeroPar = Number(prompt("Ingrese un número par"));
+let numeroImpar = Number(prompt("Ingrese un número Impar"));
+let resultado1 = nombre   + " " + apellido;
+let resultado2 = numeroPar + numeroImpar;
+
+alert ("Hola " + resultado1 +"!" + " " + "Tu resultado es " +  resultado2 );
